25a970bfff4c2a386c8bb70728e091b24430b577
karnikkanojia <karnikk1406120@gmail.com>
2022-01-05 18:43:15 +0530

    Server initial setup done.
    Routes made and tested with POSTMAN.
    Connection with MONGODB ATLAS setup done.
    Working

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express()
+const port = 3000;
+const tasks = require('./routes/tasks');
+const { connectDB } = require('./db/connect');
+require('dotenv').config();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.end("Hello World");
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGOURL);
+        app.listen(port, () => console.log('Server started'));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
